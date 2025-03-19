281e9345ce480530d7dcec9f8673f329ce27ab5c
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2025-03-19 10:40:35 +0000

    contact creation

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.app')
 @section('content')
-
+<div class="w-4/5 m-auto text-center">
+    <div class="py-16">
+        <h1 class="text-4xl font-semibold text-custom-dark-blue font-custom no-underline">
+            Contact Me
+        </h1>
+    </div>
+</div>
 @endsection
