df00ba1d8b21d10a1a81e6a8774ed3fcc48257f3
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2025-03-23 18:45:38 +0000

    back button on gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -1,44 +1,22 @@
 @extends('layouts.app')
 @section('content')
-<div class="w-4/5 m-auto text-center">
-    <div class="pt-16 pb-7">
+<div class="w-4/5 m-auto text-left">
+    <div class="py-5">
+        <a href="{{ url('/') }}" class="text-custom-purple-2 hover:text-custom-purple-3 text-3xl">
+            <i class="fas fa-arrow-left"></i> <!-- Icône de flèche -->
+        </a>
+    </div>
+    <div class="pt-16 pb-7 text-center">
         <h1 class="text-4xl font-semibold text-custom-dark-blue font-custom no-underline">
             Gallery
         </h1>
     </div>
 </div>
 <div class="w-4/5 m-auto text-center">
-    
     <p class="text-custom-dark-blue">
-  This gallery is full of inspiration from pinterest, new inspi will arrived soon. click on the picture to access the pinterest link.
-  </p>
-    
+        This gallery is full of inspiration from Pinterest, new inspiration will arrive soon. Click on the picture to access the Pinterest link.
+    </p>
 </div>
-
-<!-- Formulaire pour télécharger des images -->
- <!--
-<div class="w-4/5 m-auto pt-10">
-    <form action="{{ route('gallery.upload') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="mb-4">
-            <label for="image" class="block text-gray-700 text-sm font-bold mb-2">Upload Image:</label>
-            <input type="file" name="image" id="image" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label for="alt_text" class="block text-gray-700 text-sm font-bold mb-2">Alt Text:</label>
-            <input type="text" name="alt_text" id="alt_text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <label for="link" class="block text-gray-700 text-sm font-bold mb-2">Link:</label>
-            <input type="url" name="link" id="link" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-        </div>
-        <div class="mb-4">
-            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Upload
-            </button>
-        </div>
-    </form>
-</div>-->
 
 <section id="gallery" class="gallery">
   <div class="container m-auto">
@@ -62,7 +40,7 @@
         <img id="modalImage" src="" alt="" class="w-full h-auto rounded-lg">
       </div>
       <div class="items-center px-4 py-3">
-        <button id="openImageButton" class="px-4 py-2 bg-custom-purple-2 hover:bg-custom-purple-3  text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
+        <button id="openImageButton" class="px-4 py-2 bg-custom-purple-2 hover:bg-custom-purple-3 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
           Open Link in New Tab
         </button>
         <button onclick="closeModal()" class="mt-2 px-4 py-2 bg-gray-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-300">
@@ -86,6 +64,5 @@
     document.getElementById('imageModal').classList.add('hidden');
   }
 </script>
-
 
 @endsection
